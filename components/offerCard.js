37636ffa99eb87d7a54d3729cf7be36c84fa3fc2
funnyMe214 <163763311+funnyMe214@users.noreply.github.com>
2024-08-24 15:14:37 -0700

    Update offerCard.js

--- components/offerCard.js
+++ components/offerCard.js
@@ -4,7 +4,7 @@ const OfferCard = ({ imageSrc, offerCode, description }) => {
   return (
     <div className="offer-card col-md-6 mb-4 mx-10">
       <div className="offer-image-container">
-        <Image src={imageSrc} alt="Offer" className="offer-image img-thumbnail"/>
+        <Image src={imageSrc} alt="Offer" width={526} height={296} className="offer-image img-thumbnail"/>
         <div className="copyable-text-bottom">
           <CopyableText text={offerCode} />
         </div>
