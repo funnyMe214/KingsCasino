84732ce593c7f07369072073fe2604f1a27872bb
funnyMe214 <163763311+funnyMe214@users.noreply.github.com>
2024-08-24 14:26:01 -0700

    Update offerCard.js

--- components/offerCard.js
+++ components/offerCard.js
@@ -1,10 +1,10 @@
 import CopyableText from './CopyableText';
-
+import Image from 'next/image';
 const OfferCard = ({ imageSrc, offerCode, description }) => {
   return (
     <div className="offer-card col-md-6 mb-4 mx-10">
       <div className="offer-image-container">
-        <img src={imageSrc} alt="Offer" className="offer-image img-thumbnail"/>
+        <Image src={imageSrc} alt="Offer" className="offer-image img-thumbnail"/>
         <div className="copyable-text-bottom">
           <CopyableText text={offerCode} />
         </div>
