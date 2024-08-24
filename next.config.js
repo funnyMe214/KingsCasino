9f20260542f3e738b904b9e549578af5bf30d9e2
funnyMe214 <163763311+funnyMe214@users.noreply.github.com>
2024-08-24 15:10:07 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     env: {
       DATABASE_URL: process.env.DATABASE_URL,
     },
+     images: {
+    domains: ['play-lh.googleusercontent.com', 's.cafebazaar.ir', 'i.pinimg.com', 'encrypted-tbn0.gstatic.com', 'img1.wsimg.com'],
+  },
   };
   
   module.exports = nextConfig;
