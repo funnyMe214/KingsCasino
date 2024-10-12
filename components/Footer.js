161dfd32d115335610fcf40c3adefc09fe3e4609
funnyMe214 <163763311+funnyMe214@users.noreply.github.com>
2024-10-13 03:57:01 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,16 @@
 // components/Footer.js
+
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3 mt-auto " id="footer">
-      <p>Follow us on 
-        <a href="https://www.facebook.com/KiingTreasure" target="_blank" className="text-white"> Facebook</a>, 
-        <a href="#" target="_blank" className="text-white"> Twitter</a>, 
-        <a href="#" target="_blank" className="text-white"> Instagram</a>
+    <footer className="bg-dark text-white text-center py-3 mt-auto" id="footer">
+      <p>
+        Follow us on 
+        <a href="https://www.facebook.com/KiingTreasure" target="_blank" rel="noopener noreferrer" className="text-white"> Facebook</a>, 
+        <a href="#" target="_blank" rel="noopener noreferrer" className="text-white"> Twitter</a>, 
+        <a href="#" target="_blank" rel="noopener noreferrer" className="text-white"> Instagram</a>
+      </p>
+      <p>
+        <a href="/privacy-policy" className="text-white">Privacy Policy</a>
       </p>
     </footer>
   );
